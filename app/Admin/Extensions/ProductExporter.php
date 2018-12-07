820301ac5d9fb02ac3ac4382cc7856433d406762
fengtian <tayami_fighting@yeah.net>
2018-12-07 18:59:00 +0800

    1

--- app/Admin/Extensions/ProductExporter.php
+++ app/Admin/Extensions/ProductExporter.php
@@ -31,13 +31,20 @@ class ProductExporter extends AbstractExporter
         Excel::create($cate, function($excel) use ($exceptFields) {
 
             $excel->sheet('export data', function($sheet) use ($exceptFields) {
-
+                $exceptFields = array_merge($exceptFields, ['id', 'images', 'state', 'deleted_at']);
                 // 这段逻辑是从表格数据中取出需要导出的字段
+                $keys = [];
+                if (isset($this->getData(false)[0])) {
+                    $data = json_decode(json_encode($this->getData(false)[0]), true);
+                    $keys = array_keys($data);
+                    $keys = array_diff($keys, $exceptFields);
+                }
                 $rows = collect($this->getData())->map(function ($item) use ($exceptFields) {
-                    $exceptFields = array_merge($exceptFields, ['id', 'images', 'state']);
                     return array_except($item, $exceptFields);
                 });
 
+                $rows = $rows->toArray();
+                array_unshift($rows, $keys);
                 $sheet->rows($rows);
 
             });
