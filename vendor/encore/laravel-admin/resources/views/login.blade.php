5dde05e6356edf1518f1517b4abbb42d62fd3151
fengtian <tayami_fighting@yeah.net>
2019-07-02 02:10:06 +0800

    update products

--- vendor/encore/laravel-admin/resources/views/login.blade.php
+++ vendor/encore/laravel-admin/resources/views/login.blade.php
@@ -23,7 +23,7 @@
   <![endif]-->
 </head>
 <body class="hold-transition login-page" @if(config('admin.login_background_image'))style="background: url({{config('admin.login_background_image')}}) no-repeat center top;background-size: cover;"@endif>
-<div class="login-box">
+<div class="login-box" style="margin-top: 250px;">
   <div class="login-logo">
     <a href="{{ admin_base_path('/') }}"><b>{{config('admin.name')}}</b></a>
   </div>
